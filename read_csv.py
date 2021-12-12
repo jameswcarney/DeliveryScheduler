87c30e83d04f9b85b2c2b7c6e168984417649440
jamiecello <jamiecello@gmail.com>
2021-12-11 22:36:17 -0700

    Moved loaded data to globals.py

--- read_csv.py
+++ read_csv.py
@@ -1,10 +1,9 @@
 import csv
+import globals
 from objects import Package
-from hash_table import HashTable
-
-packageHash = HashTable()
 
 
+# Load package data from packages.csv and insert it into a hash table
 def load_package_data(file_name):
     with open(file_name) as packages:
         package_data = csv.reader(packages, delimiter=',')
@@ -22,15 +21,27 @@ def load_package_data(file_name):
             package = Package(package_id, address, city, state, zip, deadline, weight, notes, status)
 
             # Insert the package into the hash table
-            packageHash.insert(package_id, package)
+            globals.packageHash.insert(package_id, package)
 
 
+# Load weighted distance data from distances.csv and use it to create an adjacency list
 def load_adjacency_data(file_name):
     with open(file_name) as distances:
         adjacency_data = list(csv.reader(distances, delimiter=','))
+        for node in adjacency_data:
+            globals.adjacency_list.append(node)
+
+
+# Load address data from addresses.csv to create an address list
+def load_address_data(file_name):
+    with open(file_name) as addresses:
+        address_data = csv.reader(addresses, delimiter=',')
+        for address in address_data:
+            globals.address_list.append(address[1])
 
 
 load_package_data('./data/packages.csv')
 load_adjacency_data('./data/distances.csv')
+load_address_data('./data/addresses.csv')
 
 
